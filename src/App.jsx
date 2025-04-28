532871b4d62239079d27f7df5c2291ea1a60bc70
Raditya Dahal <dahalraditya5511@gmail.com>
2025-04-28 10:42:49 +0300

    Change

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import TodoTable from './TodoTable';
 import './App.css'
 
 function App() {
@@ -15,7 +16,7 @@ function App() {
   }
 
   const deleteTodo = (row) => {
-    console.log('Delete row:' + row);
+    setTodos(todos.filter((todo, index)=> index !== row));
   }
  
 
@@ -24,19 +25,7 @@ function App() {
       <input placeholder="Description" name="description" value={todo.description} onChange={inputChanged} />
       <input placeholder="Date" name="date" value={todo.date} onChange={inputChanged} />
       <button onClick={addTodo}>Add</button>
-
-      <table>
-        <tbody>
-          {
-            todos.map((todo, index) =>
-              <tr key={index}>
-                <td>{todo.description}</td>
-                <td>{todo.date}</td>
-                <td><button onClick={()=> deleteTodo(index)}>Delete</button></td>
-              </tr>)
-          }
-        </tbody>
-      </table>
+      <TodoTable todos={todos} deleteTodo={deleteTodo} />
     </>
   );
 }
