8ce6c6cb0e5729a6b501fee554992cf8b01ef10b
Raditya Dahal <dahalraditya5511@gmail.com>
2025-04-28 10:30:42 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+import './App.css'
+
+function App() {
+  const [todo, setTodo] = useState({ description: '', date: '' });
+  const [todos, setTodos] = useState([]);
+
+  const inputChanged = (event) => {
+    setTodo({ ...todo, [event.target.name]: event.target.value });
+  }
+
+  const addTodo = () => {
+    setTodos([...todos, todo]);
+    setTodo({ description: '', date: '' }); // Clear todo
+  }
+
+  const deleteTodo = (row) => {
+    console.log('Delete row:' + row);
+  }
+ 
+
+  return (
+    <>
+      <input placeholder="Description" name="description" value={todo.description} onChange={inputChanged} />
+      <input placeholder="Date" name="date" value={todo.date} onChange={inputChanged} />
+      <button onClick={addTodo}>Add</button>
+
+      <table>
+        <tbody>
+          {
+            todos.map((todo, index) =>
+              <tr key={index}>
+                <td>{todo.description}</td>
+                <td>{todo.date}</td>
+                <td><button onClick={()=> deleteTodo(index)}>Delete</button></td>
+              </tr>)
+          }
+        </tbody>
+      </table>
+    </>
+  );
+}
+
+export default App
